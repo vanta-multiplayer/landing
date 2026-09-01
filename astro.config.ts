e5ceed8cdee98d4357a8fa397e70601e2f467253
Norbert Podbiegly <78374441+jacuniadev@users.noreply.github.com>
2026-09-01 08:58:00 +0200

    add deploy workflow for GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://vantamp.xyz",
+  base: "/",
+  output: "static",
+
   integrations: [solid()],
+
   vite: {
     plugins: [tailwindcss()],
   },
